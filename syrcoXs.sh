69713a7a003df7f8e93c3aecc834595e3df2615a
Alexander Desyatnichenko <1_0@usa.com>
2011-06-28 23:51:58 +0300

    server get parse

--- syrcoXs.sh
+++ syrcoXs.sh
@@ -19,10 +19,30 @@
       #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
       #MA 02110-1301, USA.
 
-( echo '----------------'; date;  set -o posix ; set ; echo '====================' ; echo '0'; echo $0; echo '1'; echo $1; echo '2'; echo $2; echo '*'; echo $*; echo '====================' ) > ./syr.log
+#while read data; do
+    #echo "data = $data"
+    #date
+#done
 
-echo 'HTTP/1.0 302 Found'
-#echo 'Location: http://ya.ru/'
+
+#i="0"
+
+#while [ $i -lt 6 ]
+#do
+#read data
+#i=$[$i+1]
+#echo "iii = $i"
+  
+    #echo "data = $data"
+    #date
+#done
+
+read -r ddata
+
+#( echo '----------------'; date;  set -o posix ; set ; echo '====================' ; echo '0'; echo $0; echo '1'; echo $1; echo '2'; echo $2; echo '*'; echo $*; echo '====================' ) > ./syr.log
+
+echo 'HTTP/1.0 200 OK'
+#echo 'Location: http://127.0.0.1:33222/'
 #echo 'Cache-Control: private'
 echo 'Content-Type: text/html; charset=UTF-8'
 echo 'X-Content-Type-Options: nosniff'
@@ -36,8 +56,21 @@ echo '<HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf
 echo '<TITLE>HI SYRCOX</TITLE></HEAD><BODY>'
 echo '<H1>syrcoX</H1>'
 echo 'Hello to all'
-echo '<A HREF="http://127.0.0.1/">home</A>.<br><h1>'
+echo '<A HREF="http://127.0.0.1:33222/">home</A>.<br><h1>'
 date
+echo $ddata | sed '/^GET */!d; s/ HTTP\/1.1//;q'| sed '/^GET */!d; s///;q'
+echo '<br>--------<br>'
+
+filee=$(echo $ddata | sed '/^GET */!d; s/ HTTP\/1.1//;q'| sed '/^GET */!d; s///;q')
+echo '<br>======<br>'
+pwd
+echo '<br>--------<br>'
+ls -l ./
+echo '<br>--------<br>'
+cat .${filee}
+echo '<br>--------<br>'
+`.${filee}`
+
 echo '</h1></BODY></HTML>'
 
 echo '</h1></html>'
